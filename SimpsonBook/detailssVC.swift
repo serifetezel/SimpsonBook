9538b67ee2c859f0f9a6fcee1d63cdb100783847
Şerife Tezel <serifetezel15@gmail.com>
2023-09-11 22:25:33 +0300

    simpson app done

---
+++ SimpsonBook/detailssVC.swift
@@ -0,0 +1,29 @@
+//
+//  detailssVC.swift
+//  SimpsonBook
+//
+//  Created by MacbookPro on 10.08.2023.
+//
+
+import UIKit
+
+class detailssVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
